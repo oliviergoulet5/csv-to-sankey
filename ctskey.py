8ce758e69630b98339002518456f86bbc18fb249
Olivier Goulet <olivier_goulet@icloud.com>
2022-04-22 15:18:57 -0400

    Rename file

--- ctskey.py
+++ ctskey.py
@@ -5,6 +5,10 @@ import csv
 from clearOutDir import clearOutDir
 from writeToSankey import writeToSankey
 
+ylw_underline = "\033[4;33m"
+ylw = "\033[0;33m"
+w = "\033[0;37m"
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-f", "--file", dest="file", help="CSV file")
@@ -20,6 +24,11 @@ def main():
 
             for row in csv_file:
                 writeToSankey(row)
+
+        
+        print(f"💎 {ylw}ctskey owes credit to the makers of SankeyMATIC. Consider donating to them!")
+        print("\t🔗 https://sankeymatic.com/about")
+        print(f"{w}Created sankey.skmt in the out directory. ")
     return
 
 if __name__ == "__main__":
